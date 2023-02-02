16d3c2910b6f3f6e71665224a9ba17f918723673
Jonathan <93285753+JaythegamerA@users.noreply.github.com>
2023-02-01 17:45:19 -0800

    updated Debugging

--- ConsoleCode/IntroToCpp/IntroCPP_BinaryFileIO_NPCData/DataFile.cpp
+++ ConsoleCode/IntroToCpp/IntroCPP_BinaryFileIO_NPCData/DataFile.cpp
@@ -1,5 +1,6 @@
 #include "DataFile.h"
 #include <fstream>
+#include <iostream>
 using namespace std;
 
 DataFile::DataFile()
@@ -21,45 +22,44 @@ void DataFile::AddRecord(string imageFilename, string name, int age)
 	r->name = name;
 	r->age = age;
 
-	records.push_back(r);
 	recordCount++;
 }
 
 DataFile::Record* DataFile::GetRecord(int index)
 {
-	return records[index];
+	Load("npc_data.dat", index);
+	return record;
 }
 
 void DataFile::Save(string filename)
 {
 	ofstream outfile(filename, ios::binary);
 
-	int recordCount = records.size();
 	outfile.write((char*)&recordCount, sizeof(int));
 
 	for (int i = 0; i < recordCount; i++)
-	{		
-		Color* imgdata = GetImageData(records[i]->image);
-				
-		int imageSize = sizeof(Color) * records[i]->image.width * records[i]->image.height;
-		int nameSize = records[i]->name.length();
+	{
+		Color* imgdata = GetImageData(record->image);
+
+		int imageSize = sizeof(Color) * record->image.width * record->image.height;
+		int nameSize = record->name.length();
 		int ageSize = sizeof(int);
 
-		outfile.write((char*)&records[i]->image.width, sizeof(int));
-		outfile.write((char*)&records[i]->image.height, sizeof(int));
-		
+		outfile.write((char*)&record->image.width, sizeof(int));
+		outfile.write((char*)&record->image.height, sizeof(int));
+
 		outfile.write((char*)&nameSize, sizeof(int));
 		outfile.write((char*)&ageSize, sizeof(int));
 
 		outfile.write((char*)imgdata, imageSize);
-		outfile.write((char*)records[i]->name.c_str(), nameSize);
-		outfile.write((char*)&records[i]->age, ageSize);
+		outfile.write((char*)record->name.c_str(), nameSize);
+		outfile.write((char*)&record->age, ageSize);
 	}
 
 	outfile.close();
 }
 
-void DataFile::Load(string filename)
+void DataFile::Load(string filename, int index)
 {
 	Clear();
 
@@ -68,49 +68,75 @@ void DataFile::Load(string filename)
 	recordCount = 0;
 	infile.read((char*)&recordCount, sizeof(int));
 
-	for (int i = 0; i < recordCount; i++)
-	{		
-		int nameSize = 0;
-		int ageSize = 0;
-		int width = 0, height = 0, format = 0, imageSize = 0;
+	int nameSize = 0;
+	int ageSize = 0;
+	int width = 0, height = 0, format = 0, imageSize = 0;
 
-		infile.read((char*)&width, sizeof(int));
-		infile.read((char*)&height, sizeof(int));
+	int widthOffset = 0;
+	int heightOffset = 0;
+	int imageSizeOffset = 0;
+	int size = sizeof(int);
+	for (int i = 0; i < index; i++)
+	{
+		int tempWidth = 0;
+		int tempHeight = 0;
+		int tempNameSize = 0;
+		int tempAgeSize = 0;
+		infile.read((char*)&tempWidth, sizeof(int));
+		infile.read((char*)&tempHeight, sizeof(int));
+		infile.read((char*)&tempNameSize, sizeof(int));
+		infile.read((char*)&tempAgeSize, sizeof(int));
 
-		imageSize = sizeof(Color) * width * height;
+		// Get where we are in the file and set size to that
+		size = infile.tellg();
 
-		infile.read((char*)&nameSize, sizeof(int));
-		infile.read((char*)&ageSize, sizeof(int));
+		// Add image, name, and age sizes to the size variable
+		size += sizeof(Color) * tempWidth * tempHeight;
+		size += tempNameSize + tempAgeSize;
 
-		char* imgdata = new char[imageSize];
-		infile.read(imgdata, imageSize);
-
-		Image img = LoadImageEx((Color*)imgdata, width, height);
-		char* name = new char[nameSize];
-		int age = 0;
-				
-		infile.read((char*)name, nameSize);
-		infile.read((char*)&age, ageSize);
-
-		Record* r = new Record();
-		r->image = img;
-		r->name = string(name);
-		r->age = age;
-		records.push_back(r);
-
-		delete [] imgdata;
-		delete [] name;
+		// Shift the file location over to where the size is
+		infile.seekg(size);
 	}
+
+	std::cout << size << std::endl;
+
+	infile.seekg(size, std::ios::beg);
+	infile.read((char*)&width, sizeof(int)); // FIXED
+	infile.read((char*)&height, sizeof(int)); // FIXED
+
+	imageSize = sizeof(Color) * width * height;
+
+	infile.read((char*)&nameSize, sizeof(int)); // FIXED
+	infile.read((char*)&ageSize, sizeof(int)); // FIXED
+
+	char* imgdata = new char[imageSize];
+	infile.read(imgdata, imageSize); // VARIABLE
+
+	Image img = LoadImageEx((Color*)imgdata, width, height);
+	char* name = new char[nameSize + 1]; // Add one to the nameSize for the actual array size to make room for the null terminator
+	int age = 0;
+
+	infile.read((char*)name, nameSize); // VARIABLE
+	infile.read((char*)&age, ageSize); // VARIABLE
+
+	std::cout << infile.tellg() << std::endl;
+
+	// Add the null terminator
+	name[nameSize] = '\0';
+
+	record = new Record();
+	record->image = img;
+	record->name = string(name);
+	record->age = age;
+
+	delete[] imgdata;
+	delete[] name;
 
 	infile.close();
 }
 
 void DataFile::Clear()
 {
-	for (int i = 0; i < records.size(); i++)
-	{
-		delete records[i];
-	}
-	records.clear();
+	delete record;
 	recordCount = 0;
 }
--- ConsoleCode/IntroToCpp/IntroCPP_BinaryFileIO_NPCData/DataFile.h
+++ ConsoleCode/IntroToCpp/IntroCPP_BinaryFileIO_NPCData/DataFile.h
@@ -17,7 +17,8 @@ public:
 
 private:
 	int recordCount;
-	std::vector<Record*> records;	// delete this vector. Load only the required record 
+	Record* record;
+	//std::vector<Record*> records;	// delete this vector. Load only the required record 
 
 
 
@@ -28,14 +29,14 @@ public:
 	void AddRecord(string imageFilename, string name, int age);
 	Record* GetRecord(int index);
 
-	int GetRecordCount() { return recordCount; };
+	// Subtract 1 from the record count so that when you press the right arrow
+	// You'll never get a crash when you're on the last record
+	int GetRecordCount() { return recordCount - 1; };
 
 	void Save(string filename);
-	void Load(string filename);
+	void Load(string filename, int index);
 
 private:
 	void Clear();
 
 };
-
-
